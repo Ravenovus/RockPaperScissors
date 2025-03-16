72d2e0e401ff835751198f37f994d91c6389e831
Ravenovus <voronov213@gmail.com>
2025-03-16 09:43:21 +0200

    set up Human choice prompt and conversion to number

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ function getComputerChoice(){
     return (Math.trunc((Math.random()*100)%3));
 }
 
+function getHumanChoice(){
+    let choice = prompt("Chose now!");
+    let answer = 0;
+    if(choice == choices[0]){
+        answer = 0;
+    }
+    else if(choice == choices[1]){
+        answer = 1;
+    }
+    else if(choice == choices[2]){
+        answer = 2;
+    }
+    return answer;
+}
+
 const choices = ["Rock", "Paper", "Scissors"];
-console.log(choices[getComputerChoice()]);
-console.log(choices[getComputerChoice()]);
-console.log(choices[getComputerChoice()]);
-console.log(choices[getComputerChoice()]);
-console.log(choices[getComputerChoice()]);
+let humanScore = 0;
+let computerScore = 0;
