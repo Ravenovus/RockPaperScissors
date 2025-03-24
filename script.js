2dee7f3258738ab624506daf534f40b4c0264cfc
Ravenovus <voronov213@gmail.com>
2025-03-24 10:50:48 +0200

    added ui with buttons, text and removed all console messaging

--- script.js
+++ script.js
@@ -2,63 +2,77 @@ function getComputerChoice(){
     return (Math.trunc((Math.random()*100)%3));
 }
 
-function getHumanChoice(){
-    let choice = prompt("Chose now!");
-    let answer = 0;
-
-    choice = capitalize(choice);
-
-    if(choice == choices[0]){
-        answer = 0;
-    }
-    else if(choice == choices[1]){
-        answer = 1;
-    }
-    else if(choice == choices[2]){
-        answer = 2;
-    }
-    return answer;
-}
-
-function capitalize(word){
-    let tempWord = String(word).toLocaleLowerCase();
-    return tempWord.charAt(0).toUpperCase()+tempWord.slice(1);
-}
-
 function playRound(humanChoice, computerChoice){
     let winner = -1;
-    console.log("You picked: "+choices[humanChoice]+" Computer picked: "+choices[computerChoice]);
+    let playerBox = document.querySelector("#player_choice");
+    let computerBox = document.querySelector("#computer_choice");
+    let resultBox = document.querySelector("#winner_result");
+    let result ="";
+
+    playerBox.textContent = "You picked: "+ choices[humanChoice];
+    computerBox.textContent = "Computer picked: "+ choices[computerChoice];
+    
     if(humanChoice == computerChoice){
-        console.log("Tied!");
+        result = "Tied!";
         winner = 0;
     }
     else if((humanChoice + 2)%3 == computerChoice){
-        console.log("You Win! "+ choices[humanChoice]+" beats "+choices[computerChoice]+".");
+        result = "You Win! "+ choices[humanChoice]+" beats "+choices[computerChoice]+".";
         winner = 1;
     }
     else{
-        console.log("You Lose! "+ choices[computerChoice]+" beats "+choices[humanChoice]+".");
+        result = "You Lose! "+ choices[computerChoice]+" beats "+choices[humanChoice]+".";
         winner = 2;
     }
+
+    resultBox.textContent = result;
     return winner;
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+function playGame(humanChoice){
     let winner = -1;
 
-    for(let i=0;i<5;i++){
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        winner = playRound(humanChoice,computerChoice);
-        if (winner == 1) humanScore++;
-        else if (winner == 2) computerScore++;
-        winner = -1;
-        console.log("Current Score:\nYou: "+humanScore+"\nComputer: "+computerScore);
-    }
+    const computerChoice = getComputerChoice();
+    winner = playRound(humanChoice,computerChoice);
+    if (winner == 1) humanScore++;
+    else if (winner == 2) computerScore++;
+    winner = -1;
+    updateScores();
+}
+
+function updateScores(){
+    const playerScoreText = document.querySelector("#player_score");
+    const computerScoreText = document.querySelector("#computer_score");
+
+    playerScoreText.textContent = "Player Score: " + humanScore;
+    computerScoreText.textContent = "Computer Score: " + computerScore;
+
 }
 
 const choices = ["Rock", "Paper", "Scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
-playGame();
+
+const buttons_selector = document.querySelector(".Button_Container")
+
+buttons_selector.addEventListener('click', (event) => {
+    let target = event.target;
+    let humanChoice = -1;
+
+    switch(target.id){
+        case 'rock_button':
+            humanChoice = 0;
+            break;
+        case 'paper_button':
+            humanChoice = 1;
+            break;
+        case "scissors_button":
+            humanChoice = 2;
+            break;
+    }
+
+    playGame(humanChoice);
+})
+
+updateScores();
